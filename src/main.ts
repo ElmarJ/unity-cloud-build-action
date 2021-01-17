57f2d2bb1b6179c8f2af0089d3646b0aa91af0bb
Elmar Jansen <elmar@elmarjansen.nl>
2021-01-17 14:50:16 +0100

    Remove api-library, use axios to call API

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,43 @@
 import * as core from '@actions/core'
-import * as unity from './api_library/api'
+import * as github from '@actions/github'
+import * as axios from 'axios'
 
 async function run(): Promise<void> {
   try {
     const orgid: string = core.getInput('orgid')
     const projectid: string = core.getInput('projectid')
     const buildtargetid: string = core.getInput('buildtargetid')
-    const unityBuilds = new unity.BuildsApi()
+    const apiKey = core.getInput('apiKey')
+    const apiUrl = 'https://build-api.cloud.unity3d.com/api/v1'
+
+    const startBuildEndpoint = `/orgs/${orgid}/projects/${projectid}/buildtargets/${buildtargetid}/builds`
+    const startBuildData = {
+      clean: false,
+      delay: 0,
+      commit: github.context.sha,
+      headless: false,
+      label: '',
+      platform: 'linux'
+    }
+    const requestOptions = {
+      headers: {
+        Authentication: `Basic ${apiKey}`
+      }
+    }
 
     core.debug('start')
     core.debug(new Date().toTimeString())
 
-    var response = await unityBuilds.startBuilds(
-      orgid,
-      projectid,
-      buildtargetid
+    const response = await axios.default.post(
+      apiUrl + startBuildEndpoint,
+      startBuildData,
+      requestOptions
     )
-    var buildResult = response.body[0]
+
+    const buildResult = response.data[0]
 
     core.debug(`Build finished in ${buildResult.buildTimeInSeconds} seconds.`)
-    if (
-      buildResult.buildStatus !=
-      unity.OrgsOrgidProjectsProjectidBuildtargetsBuilds.BuildStatusEnum.Success
-    ) {
+    if (buildResult.buildStatus !== 'success') {
       core.setFailed(
         `Build failed with status ${buildResult.buildStatus}. Message: ${buildResult.failureDetails}`
       )
